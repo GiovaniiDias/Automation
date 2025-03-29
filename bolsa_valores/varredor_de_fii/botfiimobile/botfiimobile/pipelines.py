891376d43018f53c19dd3975563391e31c1fbaca
Giovani Dias <giovani.eb3@gmail.com>
2025-03-29 16:51:00 -0300

    Bot para varrer site de fundos imobiliarios e selecionar os melhores fundos, com base nos filtros

---
+++ bolsa_valores/varredor_de_fii/botfiimobile/botfiimobile/pipelines.py
@@ -0,0 +1,30 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import openpyxl
+from botfiimobile.settings import XLSX_PATH
+
+CAMPOS = ['Papel','Segmento','Dividend Yield','P/VP','Valor de Mercado','Liquidez']
+class BotfiimobilePipeline(object):
+    planilha = None
+    sheet = None
+
+    def open_spider(self,spider):
+        self.planilha = openpyxl.Workbook()
+        self.sheet =self.planilha.active
+        self.sheet.append(CAMPOS)
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        self.sheet.append([adapter.get('Papel'),adapter.get('Segmento'),adapter.get('Dividend Yield'),adapter.get('P/VP'),adapter.get('Valor de Mercado'),adapter.get('Liquidez')])
+
+        return item
+    
+    def close_spider(self,spider):
+        self.planilha.save(XLSX_PATH)
+
---
+++ bolsa_valores/varredor_de_fii/botfiimobile/botfiimobile/spiders/botfii.py
@@ -0,0 +1,35 @@
+import scrapy
+import random
+
+class FiimobbotSpider(scrapy.Spider):
+    name = 'fiimobbot'
+    allowed_domains = ['fundamentus.com.br']
+    start_urls = ['https://www.fundamentus.com.br/fii_resultado.php']
+
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
+        # Adicione mais user-agents aqui
+    ]
+    custom_settings = {
+        'DOWNLOAD_DELAY': random.uniform(1, 3), # Delay aleatório entre 1 e 3 segundos
+    }
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, headers={'User-Agent': random.choice(self.user_agents)})
+
+    def parse(self, response):
+        yield scrapy.Request(response.url, callback=self.parse_data, dont_filter=True, headers={'User-Agent': random.choice(self.user_agents)})
+
+    def parse_data(self, response):
+        for linha in response.xpath("//table[@id='tabelaResultado']//tr[position() > 1]"): #pula o cabeçalho
+            yield {
+                'Papel': linha.xpath('./td[1]//a/text()').get(default='N/A'),
+                'Segmento': linha.xpath('./td[2]/text()').get(default='N/A'),
+                'Dividend Yield': linha.xpath('./td[5]/text()').get(default='N/A'),
+                'P/VP': linha.xpath('./td[6]/text()').get(default='N/A'),
+                'Valor de Mercado': linha.xpath('./td[7]/text()').get(default='N/A'),
+                'Liquidez': linha.xpath('./td[8]/text()').get(default='N/A'),
+            }
